b2b56146367fe0d415aaf4c5e468d6a5b1b1d150
nicogid <nicolas.gidon@gmail.com>
2017-06-16 15:46:10 +0200

    user GET+PUT+DELETE

--- api/projet/distributor_api/urls.py
+++ api/projet/distributor_api/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^$', views.home_test),
-    url(r'^users', views.user_list,name="user_list"),
+    url(r'^users', views.user_list,name="user-list"),
+    url(r'^users/(?P<pk>[0-9]+)/$',views.user_detail,name="user-detail"),
 ]
--- api/projet/distributor_api/views.py
+++ api/projet/distributor_api/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
+from rest_framework.exceptions import ParseError
+from rest_framework.parsers import JSONParser
 
 from .models import User
 from .serializers import UserSerializer
@@ -19,3 +21,35 @@ def user_list(request):
         users = User.objects.all()
         serializer = UserSerializer(users,many= True)
         return JsonResponse(serializer.data,safe=False,status=status.HTTP_200_OK)
+
+
+@csrf_exempt
+def user_detail(request,pk):
+    try:
+        user =User.objects.get(pk)
+    except User.DoesNotExist:
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = UserSerializer(user)
+        return JsonResponse(serializer.data,status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        try:
+            data = JSONParser().parse(request)
+        except ParseError:
+            return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
+        serializer =UserSerializer(user,data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data,status=status.HTTP_200_OK)
+        else:
+            return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method =='DELETE':
+        user.delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+
+    else:
+        return HttpResponse(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+
