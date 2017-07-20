c9c315e206de455c59c589fc6a6973be59b4c184
nicogid <nicolas.gidon@gmail.com>
2017-07-20 07:22:01 +0200

    apply gitignore

--- app/MyRealm/app/build/generated/source/apt/debug/io/realm/TagRealmProxyInterface.java
+++
@@ -1,9 +0,0 @@
-package io.realm;
-
-
-public interface TagRealmProxyInterface {
-    public String realmGet$name();
-    public void realmSet$name(String value);
-    public long realmGet$id();
-    public void realmSet$id(long value);
-}
